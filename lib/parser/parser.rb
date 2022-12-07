36655dff093b623008920a6620785336f0fe7b8e
husita-h <taisei.fukushi1129@gmail.com>
2022-12-07 22:58:09 +0900

    要素取得時の例外処理追加

--- lib/parser/parser.rb
+++ lib/parser/parser.rb
@@ -18,24 +18,44 @@ module Parser
     end
 
     def parsed_nodes(nodes)
-      # binding.pry
       nodes.xpath(x_path)
     end
     
-    # @param nodes [String] nodes
+    # @param nodes [Nokogiri::HTML4::Document] nodes
     # @return [Array] xmlとして出力したい要素を格納
     def extract_nodes(nodes)
-      # binding.pry
-      parsed_nodes(nodes).map do |node|
-        hash = {}
-        path = node.children[1].children[1].attribute("href").value
-        hash[:path] = path
+      nokogiri_node_sets = parsed_nodes(nodes)[0].children
+      hash = {}
+
+      nokogiri_node_sets.map.with_index do |node_set, i|
+        next if node_set.class == Nokogiri::XML::Text
+
+        hash[:path]  = extract_title(node_set.children[i])
+        hash[:title] = extract_title(node_set.children[i])
+        hash[:date]  = extract_date(node_set.children[i])
         hash
       end
     end
 
+    def extract_path(element)
+      element.attribute("href").value
+    rescue StandardError
+      "failed to extract paths."
+    end
+    
+    def extract_title(element)
+      element.children[1].children[1].children[1].children[3].children[0]
+    rescue StandardError
+      "failed to extract title."
+    end
+    
+    def extract_date(element)
+      element.children[1].children[1].children[1].children[5].children[0]
+    rescue StandardError
+      "failed to extract date."
+    end
+
     def x_path
-      # '//*[@id="main-column"]/div[1]/div[2]/div[4]/ul[1]'
       '//*[@id="article-category-list"]/ul'
     end
   end
